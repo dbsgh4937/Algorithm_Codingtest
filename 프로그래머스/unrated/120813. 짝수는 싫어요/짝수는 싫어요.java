8035bf66bf5f4cf26e9cc0ee517460557199a9ec
dbsgh4937 <130148224+dbsgh4937@users.noreply.github.com>
2023-11-24 17:03:02 +0900

    [level unrated] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n + 1) / 2];
+        int i;
+        
+        for(i = 0; i <= n; i++){
+            if(i % 2 == 1){
+                answer[i / 2] = i;
+            }
+        }           
+        
+        return answer;
+    }
+}
