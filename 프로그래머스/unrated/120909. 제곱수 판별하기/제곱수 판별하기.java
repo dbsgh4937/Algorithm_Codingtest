08320a3e88632d72a533956d57f95fa15eb7076a
dbsgh4937 <130148224+dbsgh4937@users.noreply.github.com>
2023-11-24 18:00:26 +0900

    [level unrated] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/unrated/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        if(Math.sqrt(n) % 1 == 0) {
+        if(n % Math.sqrt(n) == 0) {
             answer = 1;
         }
         else{
