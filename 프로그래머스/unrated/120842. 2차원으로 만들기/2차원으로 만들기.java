1827125bb0ef194ebec4b96814f7797ba4074e22
dbsgh4937 <130148224+dbsgh4937@users.noreply.github.com>
2023-11-25 17:53:45 +0900

    [level unrated] Title: 2차원으로 만들기, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] solution(int[] num_list, int n) {
+        int[][] answer = new int[num_list.length / n][n];
+        int i, j;
+        int count = 0;
+        
+        for(i = 0; i < (num_list.length / n); i++){
+            for(j = 0; j < n; j++){
+                answer[i][j] = num_list[count++];
+            }
+        }
+        
+        return answer;
+    }
+}
