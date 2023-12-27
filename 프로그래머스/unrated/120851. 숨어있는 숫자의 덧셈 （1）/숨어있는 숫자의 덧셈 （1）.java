6cd50cba82c78d37b2caa2298d3ac0a98d0595a1
dbsgh4937 <130148224+dbsgh4937@users.noreply.github.com>
2023-12-28 00:13:27 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.22 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/unrated/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -2,11 +2,14 @@ class Solution {
     public int solution(String my_string) {
         int answer = 0;
         
+        //a-z, A-Z의 알파벳들을 null인 ""로 다 replace한다.
         my_string = my_string.replaceAll("[a-zA-Z]", "");
+        //arr라는 배열을 하나 만들어서 null기준으로 split한다
         String[] arr = my_string.split("");
         
         int i;
         for(i = 0; i < arr.length; i++){
+            //split된 arr에서의 정수값을 integer로 변환해서 answer에 저장
             answer += Integer.valueOf(arr[i]);
         }
         
