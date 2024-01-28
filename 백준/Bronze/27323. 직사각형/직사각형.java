52564efc2f658ced10d278fc385d889f2a18e347
dbsgh4937 <130148224+dbsgh4937@users.noreply.github.com>
2024-01-28 13:13:49 +0900

    [Bronze V] Title: 직사각형, Time: 208 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Bronze/27323. 직사각형/직사각형.java
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
 
-        System.out.println(a*b);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        System.out.println(A * B);
     }
 }
