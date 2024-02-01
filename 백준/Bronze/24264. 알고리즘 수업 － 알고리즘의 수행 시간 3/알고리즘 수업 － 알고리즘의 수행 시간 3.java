79c81fe03887a7c70d358ac13d97b42863142d5b
dbsgh4937 <130148224+dbsgh4937@users.noreply.github.com>
2024-02-02 00:21:47 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 120 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //BufferReader객체를 생성해서 텍스트를 읽어올 것이다.
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        //n * n이 되면 int 범위를 뛰어넘음
+        Long n = Long.parseLong(bufferedReader.readLine());
+        System.out.println(n * n);
+        System.out.println("2");
+    }
+}
