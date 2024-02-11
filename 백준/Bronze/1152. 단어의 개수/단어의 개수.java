ff8e3dc82e08fc430f384a4b0463dfdfdbbe919b
dbsgh4937 <130148224+dbsgh4937@users.noreply.github.com>
2024-02-12 01:12:01 +0900

    [Bronze II] Title: 단어의 개수, Time: 268 ms, Memory: 20096 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //BufferReader객체를 생성해서 텍스트를 읽어올 것이다.
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        System.out.println(stringTokenizer.countTokens());
+
+        bufferedReader.close();
+    }
+}
