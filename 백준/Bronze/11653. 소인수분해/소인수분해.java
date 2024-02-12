d5351632eb0d05be59feb5ff077fee99474f2cc9
dbsgh4937 <130148224+dbsgh4937@users.noreply.github.com>
2024-02-12 16:13:17 +0900

    [Bronze I] Title: 소인수분해, Time: 128 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -11,11 +11,17 @@ public class Main {
 
         int N = Integer.parseInt(bufferedReader.readLine());
 
-        for (int i = 2; i <= N; i++) {
+        for (int i = 2; i <= Math.sqrt(N); i++) {
             while ((N % i) == 0) {
                 stringBuilder.append(i).append("\n");
                 N /= i;
             }
+        }
+
+        //34나 39일 경우 Math.sqrt(N)으로 범위에서 나오는 값이 생략될 수 있다 17이나 13처럼
+        //이때 제일 큰 소수를 하나 출력시켜야줘야하기 때문에 하나의 조건문 설정(마지막에 하나 넣는 느낌)
+        if (N != 1) {
+            stringBuilder.append(N);
         }
 
         System.out.println(stringBuilder);
