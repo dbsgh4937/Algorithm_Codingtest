70657c06cb0042e1e925c13690cacc094169864c
dbsgh4937 <130148224+dbsgh4937@users.noreply.github.com>
2024-02-12 22:03:32 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 804 ms, Memory: 58940 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //BufferReader객체를 생성해서 텍스트를 읽어올 것이다.
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int N = Integer.parseInt(bufferedReader.readLine());
+        int[][] arr = new int[N][2];
+
+        //2차원 배열에 각각의 입력받은 값을 저장
+        StringTokenizer stringTokenizer;
+        for (int i = 0; i < N; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            //'좌표 정렬하기'와 유사하니까 입력되는 순서만 바꿔서 알고리즘은 그대로 사용
+            arr[i][1] = Integer.parseInt(stringTokenizer.nextToken());
+            arr[i][0] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+
+        //Arrays.sort 메소드를 사용해서 arr을 정렬하는데 이때 두번째 인자는 Comparator를 구현한 람다식을 사용
+        Arrays.sort(arr, (e1, e2) -> {
+            //첫번째 열이 같은지 확인해서 맞으면 두번째 열을 기준으로 오름차순 정렬한다
+            if (e1[0] == e2[0]) {
+                //두번째열을 기준으로 오름차순 정렬
+                return e1[1] - e2[1];
+            }
+            //첫번째 열이 같지 않은 경우 첫번째열을 기준으로 오름차순 정렬한다
+            else {
+                //첫번째열을 기준으로 오름차순 정렬
+                return e1[0] - e2[0];
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            stringBuilder.append(arr[i][1] + " " + arr[i][0] + "\n");
+        }
+
+        System.out.println(stringBuilder);
+
+        bufferedReader.close();
+    }
+}
