4b02988c52441f1fa6f731ed3aae86669dcbfffa
dbsgh4937 <130148224+dbsgh4937@users.noreply.github.com>
2024-02-21 01:50:55 +0900

    [Silver II] Title: 숨바꼭질 6, Time: 336 ms, Memory: 25984 KB -BaekjoonHub

--- 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.java
+++ 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -15,16 +16,19 @@ public class Main {
 
         stringTokenizer = new StringTokenizer(bufferedReader.readLine());
         for (int i = 0; i < N; i++) {
+            //동생들과의 간격을 절대값으로 산출해서 배열로 저장
             arr[i] = Math.abs(S - Integer.parseInt(stringTokenizer.nextToken()));
         }
 
-        // 모든 동생들 간의 거리 차이의 최대공약수를 구함
-        int resultMaxValue = arr[0];
+        //모든 동생들 간의 거리 차이를 통해 최대공약수를 구함
+        //동생 수(N)가 1이면 default로 하나의 거리가 최대공약수로 도출되기 위해 따로 빼고
+        //이후부터 반복문에서 GCD를 호출해서 result를 갱신해가며 최대공약수를 찾아낸다 
+        int handleResult = arr[0];
         for (int i = 1; i < N; i++) {
-            resultMaxValue = GCD(resultMaxValue, arr[i]);
+            handleResult = GCD(handleResult, arr[i]);
         }
 
-        System.out.println(resultMaxValue);
+        System.out.println(handleResult);
     }
 
     // 유클리드 호제법을 사용한 최대공약수 구하기
