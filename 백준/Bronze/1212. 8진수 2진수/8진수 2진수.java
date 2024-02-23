2f2402c0f24c4e4f8c7c89040bb1ea774fa8d80a
dbsgh4937 <130148224+dbsgh4937@users.noreply.github.com>
2024-02-23 17:14:57 +0900

    [Bronze II] Title: 8진수 2진수, Time: 2128 ms, Memory: 85104 KB -BaekjoonHub

---
+++ 백준/Bronze/1212. 8진수 2진수/8진수 2진수.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String str = bufferedReader.readLine();
+
+        //string을 8진수로 바꿔서 저장(정수로 바뀜)
+        //형 변환시 long을 넘을 수 있기 때문에 정수형을 BigInteger로 선언
+        BigInteger changeValue = new BigInteger(str, 8);
+
+        //정수형 값을 String(2진수)으로 바꾸는 방식
+        String result = changeValue.toString(2);
+
+        System.out.println(result);
+
+        bufferedReader.close();
+    }
+}
